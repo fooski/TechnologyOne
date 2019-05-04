8fd7b358501e076e5a8c960d2bafd4d44a212087
Leon zheng <leon.zheng@uqconnect.edu.au>
2019-05-04 19:00:36 +1000

    initial comit, set out class skeleton

--- src/ConvertNumbersToWords.java
+++ src/ConvertNumbersToWords.java
@@ -2,10 +2,14 @@ import java.io.Console;
 import java.util.Scanner;
 
 /**
- * placeholder
+ * The class for the program that converts input strings of numbers into words.
+ * Examples:     Input: “123.45”
+ *               Output: “ONE HUNDRED AND TWENTY-THREE DOLLARS AND FORTY-FIVE CENTS”
+ * @Author: Leon Zheng
+ * For TechnologyOne Skill Test
  */
 public class ConvertNumbersToWords {
-    String [] underTwenty = new String[] {
+    String[] underTwenty = new String[] {
             "ONE",//1
             "TWO",//2
             "THREE",//3
@@ -26,26 +30,43 @@ public class ConvertNumbersToWords {
             "EIGHTEEN",//18
             "NINETEEN"///19
     };
+
+    String[] underHundred = new String[] {
+            "TWENTY",//20
+            "THIRTY",//30
+            "FORTY",//40
+            "FIFTY",//50
+            "SIXTY",//60
+            "SEVENTY",//70
+            "EIGHTY",//80
+            "NINETY"//90
+    };
+
     public static void main(String[] arguments){
         Scanner console = new Scanner(System.in);
         System.out.println("Please enter number: ");
         String words = console.nextLine();
-        String numbers = ConvertNumbersToWords(words);
-        System.out.println("The converted number is: ");
-        System.out.println(numbers);
+        try {
+            System.out.println("The converted number is: ");
+            String numbers = ConvertNumbersToWords(words);
+            System.out.println(numbers);
+        } catch(NumberFormatException e) {
+            System.out.println("Incorrect Format!");
+            System.exit(1);
+        }
     }
 
     /**
-     * A program that converts
+     * A program that converts input strings of numbers into words.
+     * Examples:     Input: “123.45”
+     *               Output: “ONE HUNDRED AND TWENTY-THREE DOLLARS AND FORTY-FIVE CENTS”
+     * @Author: Leon Zheng
+     * For TechnologyOne Skill Test
      */
     public static String ConvertNumbersToWords(String NumberString){
         String words = new String();
-        float numbers;
-        try {
-            numbers = Float.valueOf(NumberString);
-        } catch(NumberFormatException e) {
-            System.err.println("NumberFormatException: " + e.getMessage());
-        }
+        double numbers;
+        numbers = Double.valueOf(NumberString);
         return NumberString;
     }
 }
