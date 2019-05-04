ddb376ca8b5998e3acc074d8cf13cdcc00f29e32
Leon zheng <leon.zheng@uqconnect.edu.au>
2019-05-04 19:50:31 +1000

    Implemented the ability to translate numbers under 20
    Understood the difference between static and non static methods

--- src/ConvertNumbersToWords.java
+++ src/ConvertNumbersToWords.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
  * For TechnologyOne Skill Test
  */
 public class ConvertNumbersToWords {
-    String[] underTwenty = new String[] {
+    private static String[] underTwenty = new String[] {
             "ONE",//1
             "TWO",//2
             "THREE",//3
@@ -31,7 +31,7 @@ public class ConvertNumbersToWords {
             "NINETEEN"///19
     };
 
-    String[] underHundred = new String[] {
+    private static String[] underHundred = new String[] {
             "TWENTY",//20
             "THIRTY",//30
             "FORTY",//40
@@ -58,15 +58,40 @@ public class ConvertNumbersToWords {
 
     /**
      * A program that converts input strings of numbers into words.
+     *          @param NumberString
+     *          @return
      * Examples:     Input: “123.45”
      *               Output: “ONE HUNDRED AND TWENTY-THREE DOLLARS AND FORTY-FIVE CENTS”
      * @Author: Leon Zheng
      * For TechnologyOne Skill Test
      */
     public static String ConvertNumbersToWords(String NumberString){
-        String words = new String();
+        String integerPart, decimalPart = new String();
+        if (NumberString.contains(".")) { //If the entered number has a decimal point
+            integerPart = NumberString.substring(0, NumberString.indexOf('.'));
+            decimalPart = NumberString.substring(NumberString.indexOf('.')+1,NumberString.length());
+            return NumberToWords(integerPart) + " AND " + NumberToWords(decimalPart) + " CENTS";
+        }
         double numbers;
         numbers = Double.valueOf(NumberString);
-        return NumberString;
+        return " intPart: "+NumberString;
+    }
+
+    /**
+     * Helper function which actual does the heavy lifting and converts numbers to words.
+     * Please note this function expects the string to be integers great than zero.
+     * @param numbers a integer that's great than zero
+     * @return
+     */
+    public static String NumberToWords(String numbers) {
+        String convertedNumber = new String();
+        int numbersInt = Integer.valueOf(numbers);
+        if (numbersInt < 20) {
+            return underTwenty[numbersInt - 1];
+        }
+        int ones,tens,hundreds;
+
+        ones = numbersInt % 10;
+        return convertedNumber;
     }
 }
